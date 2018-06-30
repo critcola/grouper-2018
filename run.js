edb5f8253f37f36340353d45623958350d088380
Ricky Martinez <rickmartinez4211@gmail.com>
2018-06-29 23:46:27 -0500

    init commit

---
+++ run.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const dotenv = require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);

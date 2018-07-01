61fd8916c3dbfb29db9a3c359a3c951c4c3d2278
Ricky Martinez <rickmartinez4211@gmail.com>
2018-06-30 22:59:27 -0500

    Added on voice state update

--- run.js
+++ run.js
@@ -1,16 +1,42 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv').config();
 
+const indicator = String.fromCodePoint('0x1F3AE');
+const groupIndicator = String.fromCodePoint('0x2501');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('voiceStateUpdate', function(oldMember, member) {
+
+  if(!member.voiceChannelID) {
+    return null;
   }
+
+  let channel = member.guild.channels.get(member.voiceChannelID);
+
+  if(!channel.name.startsWith(indicator)) {
+    return null;
+  }
+
+  channel.clone(groupIndicator+' Group', true)
+    .then(function(newChannel) {
+      
+      newChannel
+        .edit({
+          bitrate: 96000,
+          position: channel.position + 1,
+          userLimit: channel.userLimit
+        })
+        .then(function(newChannel) {
+          member.setVoiceChannel(newChannel)
+        });
+
+    });
+
 });
 
 client.login(process.env.DISCORD_TOKEN);

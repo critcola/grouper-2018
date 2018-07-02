09868f907e08b58e07eb54a8a768937df43e45c5
Ricky Martinez <rickmartinez4211@gmail.com>
2018-07-01 20:09:46 -0500

    Added base grouper class

--- run.js
+++ run.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv').config();
+const Grouper = require('./src/Grouper');
 
 const indicator = String.fromCodePoint('0x1F3AE');
 const groupIndicator = String.fromCodePoint('0x2501');
 
 const client = new Discord.Client();
+const grouper = new Grouper();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);

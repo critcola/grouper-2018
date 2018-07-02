a52a6f6fbde3fa330c5ab04e270f974d79386474
Ricky Martinez <rickmartinez4211@gmail.com>
2018-07-01 23:14:25 -0500

    Added position to init

--- src/Grouper.js
+++ src/Grouper.js
@@ -1,14 +1,39 @@
 const fs = require('fs');
 
 module.exports = class Grouper {
+
   constructor(client) {
     this.client = client;
   }
 
   init() {
+    let channels = [];
+    let i = 0;
+
     this.client.channels.map(function(collection) {
-      console.log(collection.type);
+      let channel = this.client.channels.get(collection.id);
+      
+      channel.edit({
+        position: i
+      });
+
+      channels.push({
+        id: collection.id,
+        type: collection.type,
+        name: collection.name,
+        position: i
+      });
+      
+      i++;
+    }, this);
+    
+    fs.writeFile('channels.json', JSON.stringify(channels, null, 2), function(error) {
+      if (error) console.log(error);
     });
+  }
+
+  moveChannel(parentChannel, childChannel) {
     
   }
+
 };
